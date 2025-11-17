724d8aae2304fcce550b16e938fcbac4f6a039b9
aarontmr <aaronthimeur@gmail.com>
2025-11-17 22:47:46 +0100

    Fix: Ajout pattern mockups dans next.config pour autoriser images du dossier mockups

--- next.config.ts
+++ next.config.ts
@@ -23,6 +23,9 @@ const nextConfig: NextConfig = {
       {
         pathname: "/devices-mockup.png",
       },
+      {
+        pathname: "/mockups/**",
+      },
     ],
     formats: ['image/avif', 'image/webp'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048],
